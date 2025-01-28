8b4cb5d6e26b61fcc5a40258f4a7a4ba5e556e1f
megaconfidence <cokoghenun@gmail.com>
2025-01-28 11:57:47 +0000

    feat: Adds pdf rendering

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,36 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.json`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import puppeteer from '@cloudflare/puppeteer';
+import { clickCmp } from 'puppeteer-cmp-clicker';
 
+// TODO:
+// ✅1. Get pdf rendering working with browser rendering
+// 2. Use email routing to deliver pdf
+// 3. Cache pdf in kv for a week
+// 4. Move implementation to workflows for async
+// 5. Keep remote browser alive with DO
+// 6. Can I use queues?
+
+interface Body {
+	url: string;
+}
+interface Env {
+	BROWSER: Fetcher;
+}
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request, env, _ctx): Promise<Response> {
+		const { url } = (await request.json()) as Body;
+		if (request.method !== 'POST' || !url) return new Response('invalid request', { status: 400 });
+		const browser = await puppeteer.launch(env.BROWSER);
+		const page = await browser.newPage();
+		await page.goto(url, {
+			waitUntil: 'networkidle2',
+		});
+		await clickCmp({ page });
+		const pdfBytes = await page.pdf({
+			format: 'letter',
+			printBackground: true,
+		});
+
+		await browser.close();
+		return new Response(pdfBytes, { headers: { 'Content-Type': 'application/pdf' } });
 	},
 } satisfies ExportedHandler<Env>;
